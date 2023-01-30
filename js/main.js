8f935dbb12fc7f540c11add6f82c9dedc0cb0ef8
abdollah <ahmadi.raya@gmail.com>
2023-01-30 23:09:24 +0330

    اصلاح چارت در صفحه اصلی cdn خط 329 عوض کردم  چند تا تغییرم تو main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+
 $(document).ready(function(){
     let RegisterUser = document.getElementById('register-user');
-    console.log(registerUser);
+    console.log(RegisterUser);
     let RegisterUserChart = new Chart(RegisterUser , {
         type : 'line',
         data : {
-            label : ['1' , '2' , '3' , '4' , '5' , '6'],
-            dataset : [{
+            labels : ['1' , '2' , '3' , '4' , '5' , '6'],
+            datasets : [{
                 data : [15333,21555,18496,24200,10600,15600],
             }]
         }
