dc025fdf9e8e3ec8c4e26f517701f72f12e4dff3
Fateme A <99539758+fati1371ahmadi@users.noreply.github.com>
2023-02-03 00:01:03 +0330

    add bar & line chart
    سلام
    چرا بردهای ایکس ووایم فالس کردمشون باز نشون داده میشه؟
    وقتی اینسپکت میگیرم قسمت کنسول بهم این پیام رو میده
    chart.js:19 Invalid scale configuration for scale: yAxes
    
    واینکه
    در فایل مین جی اس من باید حتما برای تک تک چارت هام کنسول دات لاگ رو تعریف کنم یا فقط برای چارت اول کافیه

--- js/main.js
+++ js/main.js
@@ -1,14 +1,141 @@
-
 $(document).ready(function(){
     let RegisterUser = document.getElementById('register-user');
     console.log(RegisterUser);
     let RegisterUserChart = new Chart(RegisterUser , {
         type : 'line',
         data : {
-            labels : ['1' , '2' , '3' , '4' , '5' , '6'],
+            labels : ['','','','','',''],
             datasets : [{
                 data : [15333,21555,18496,24200,10600,15600],
+                backgroundColor : 'transparent',
+                borderColor : '#fff',
+                borderWidth : 2 ,
+                pointBackgroundColor : '#fff'
             }]
+        },
+        options : {
+            layout : {
+                padding : {
+                    left : 5,
+                    right : 5
+                }
+            },
+             scales : {
+                yAxes : [{
+                  display : false  
+                }],
+                XAxes : [{
+                    display : false  
+                  }],
+             },
+             legend : {
+                display : false
+             }
+        }
+    })
+
+    let Comment = document.getElementById('comment');
+    console.log(Comment);
+    let CommentChart = new Chart(Comment , {
+        type : 'bar',
+        data : {
+            labels : ['','','','','',''],
+            datasets : [{
+                data : [15333,21555,18496,24200,10600,15600],
+                backgroundColor : '#fff',
+                borderColor : '#fff',
+                borderWidth : 2 ,
+                pointBackgroundColor : '#fff'
+            }]
+        },
+        options : {
+            layout : {
+                padding : {
+                    left : 5,
+                    right : 5
+                }
+            },
+             scales : {
+                yAxes : [{
+                  display : false  
+                }],
+                XAxes : [{
+                    display : false  
+                  }],
+             },
+             legend : {
+                display : false
+             }
+        }
+    })
+
+    let ShopUser = document.getElementById('shop-user');
+    console.log(ShopUser);
+    let ShopUserChart = new Chart(ShopUser , {
+        type : 'line',
+        data : {
+            labels : ['','','','','',''],
+            datasets : [{
+                data : [15333,21555,18496,24200,10600,15600],
+                backgroundColor : 'transparent',
+                borderColor : '#fff',
+                borderWidth : 2 ,
+                pointBackgroundColor : '#fff'
+            }]
+        },
+        options : {
+            layout : {
+                padding : {
+                    left : 5,
+                    right : 5
+                }
+            },
+             scales : {
+                yAxes : [{
+                  display : false  
+                }],
+                XAxes : [{
+                    display : false  
+                  }],
+             },
+             legend : {
+                display : false
+             }
+        }
+    })
+
+    let ApproveComment = document.getElementById('approve-comment');
+    console.log(ApproveComment);
+    let ApproveCommentChart = new Chart(ApproveComment , {
+        type : 'bar',
+        data : {
+            labels : ['','','','','',''],
+            datasets : [{
+                data : [15333,21555,18496,24200,10600,15600],
+                backgroundColor : '#fff',
+                borderColor : '#fff',
+                borderWidth : 2 ,
+                pointBackgroundColor : '#fff'
+            }]
+        },
+        options : {
+            layout : {
+                padding : {
+                    left : 5,
+                    right : 5
+                }
+            },
+             scales : {
+                yAxes : [{
+                  display : false  
+                }],
+                XAxes : [{
+                    display : false  
+                  }],
+             },
+             legend : {
+                display : false
+             }
         }
     })
 });
