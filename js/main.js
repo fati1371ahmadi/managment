1e5a47731eecfd7a516cf87e5845527c7c663be1
Fateme A <99539758+fati1371ahmadi@users.noreply.github.com>
2023-02-10 19:00:50 +0330

    result of managment

--- js/main.js
+++ js/main.js
@@ -6,30 +6,30 @@ $(document).ready(function(){
         data : {
             labels : ['','','','','',''],
             datasets : [{
-                data : [15333,21555,18496,24200,10600,15600],
-                backgroundColor : 'transparent',
-                borderColor : '#fff',
-                borderWidth : 2 ,
-                pointBackgroundColor : '#fff'
+                data : [15333,21555,18946,24200,10600,15600],
+                 backgroundColor : '#fff',
+                 borderColor : '#fff',
+                 borderWidth : 2 ,
+                 pointBackgroundColor : '#fff'
             }]
         },
         options : {
             layout : {
                 padding : {
                     left : 5,
-                    right : 5
+                    right : 5,
                 }
             },
-             scales : {
-                yAxes : [{
-                  display : false
-                }],
-                XAxes : [{
-                    display : false
-                  }],
-             },
-            plugins: {
-                legend: false
+            scales : {
+              yAxes : [{
+                display : false
+              }],
+              xAxes : [{
+                display : false
+              }],
+            },
+            plugins : {
+                legend : false
             }
         }
     })
@@ -41,31 +41,31 @@ $(document).ready(function(){
         data : {
             labels : ['','','','','',''],
             datasets : [{
-                data : [15333,21555,18496,24200,10600,15600],
-                backgroundColor : '#fff',
-                borderColor : '#fff',
-                borderWidth : 2 ,
-                pointBackgroundColor : '#fff'
+                data : [15333,21555,18946,24200,10600,15600],
+                 backgroundColor : '#fff',
+                 borderColor : '#fff',
+                 borderWidth : 2 ,
+                 pointBackgroundColor : '#fff'
             }]
         },
         options : {
             layout : {
                 padding : {
                     left : 5,
-                    right : 5
+                    right : 5,
                 }
             },
-             scales : {
-                yAxes : [{
-                  display : false  
-                }],
-                XAxes : [{
-                    display : false  
-                  }],
-             },
-             legend : {
+            scales : {
+              yAxes : [{
                 display : false
-             }
+              }],
+              xAxes : [{
+                display : false
+              }],
+            },
+            plugins: {
+                legend: false
+            }
         }
     })
 
@@ -76,66 +76,113 @@ $(document).ready(function(){
         data : {
             labels : ['','','','','',''],
             datasets : [{
-                data : [15333,21555,18496,24200,10600,15600],
-                backgroundColor : 'transparent',
-                borderColor : '#fff',
-                borderWidth : 2 ,
-                pointBackgroundColor : '#fff'
+                data : [15333,21555,18946,24200,10600,15600],
+                 backgroundColor : '#fff',
+                 borderColor : '#fff',
+                 borderWidth : 2 ,
+                 pointBackgroundColor : '#fff'
             }]
         },
         options : {
             layout : {
                 padding : {
                     left : 5,
-                    right : 5
+                    right : 5,
                 }
             },
-             scales : {
-                yAxes : [{
-                  display : false  
-                }],
-                XAxes : [{
-                    display : false  
-                  }],
-             },
-             legend : {
+            scales : {
+              yAxes : [{
                 display : false
-             }
+              }],
+              xAxes : [{
+                display : false
+              }],
+            },
+            plugins : {
+                legend : false
+            }
         }
     })
 
     let ApproveComment = document.getElementById('approve-comment');
-    console.log(ApproveComment);
+    console.log(Comment);
     let ApproveCommentChart = new Chart(ApproveComment , {
         type : 'bar',
         data : {
             labels : ['','','','','',''],
             datasets : [{
-                data : [15333,21555,18496,24200,10600,15600],
-                backgroundColor : '#fff',
-                borderColor : '#fff',
-                borderWidth : 2 ,
-                pointBackgroundColor : '#fff'
+                data : [15333,21555,18946,24200,10600,15600],
+                 backgroundColor : '#fff',
+                 borderColor : '#fff',
+                 borderWidth : 2 ,
+                 pointBackgroundColor : '#fff'
             }]
         },
         options : {
             layout : {
                 padding : {
                     left : 5,
-                    right : 5
+                    right : 5,
                 }
             },
-             scales : {
-                yAxes : [{
-                  display : false  
-                }],
-                XAxes : [{
-                    display : false  
-                  }],
-             },
-             legend : {
+            scales : {
+              yAxes : [{
                 display : false
-             }
+              }],
+              xAxes : [{
+                display : false
+              }],
+            },
+            plugins: {
+                legend: false
+            }
         }
     })
+
+    let Online = document.getElementById('online');
+    console.log(Online);
+    let OnlineChart = new Chart(Online , {
+        type : 'line',
+        data : {
+            labels : ['یکشنبه','دوشنبه','سه شنبه','چهارشنبه','پنج شنبه','جمعه'],
+            datasets : [{
+                data : [15333,21555,18946,24200,10600,15600],
+                 backgroundColor : 'rgba(0,0,0,.4)',
+                 borderColor : '#555',
+                 borderWidth : 3 ,
+                 pointBackgroundColor : '#333'
+            }]
+        },
+        options : {
+           
+            plugins: {
+                legend: false
+            }
+        }
+    })
+
+
+
+    resizeTemplate()
+    $(window).resize(resizeTemplate)
+
+
+    function resizeTemplate() {
+        if($(window).width()<=768){
+            $('#sidebar').collapse('hide');
+        } else {
+            $('#sidebar').collapse('show');
+        }
+    }
+    $('#sidebar').on('hide.bs.collapse' , function(e) {
+        if(e.target == this) {
+            $('#main').removeClass('col-md-10');
+        }
+    })
+
+    $('#sidebar').on('show.bs.collapse' , function(e) {
+        if(e.target == this) {
+            $('#main').addClass('col-md-10');
+        }
+    }) 
 });
